e1d472447ae86732fa5c085126308037986fe584
aaronlam <aaronlam1618@gmail.com>
2018-08-22 22:41:37 -0700

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Aaron Lam on 8/22/18.
+//  Copyright © 2018 Aaron Lam Developer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
